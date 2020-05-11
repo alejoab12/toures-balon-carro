3d02607bcdfbb01733474234953b7ff3e4b16554
alejoab12 <33188300+alejoab12@users.noreply.github.com>
2020-05-11 00:09:18 -0500

    Update TouresBalonCarroApplication.java

--- src/main/java/com/toures/balon/carro/TouresBalonCarroApplication.java
+++ src/main/java/com/toures/balon/carro/TouresBalonCarroApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient(autoRegister = false)
+@EnableDiscoveryClient
 public class TouresBalonCarroApplication {
 
     public static void main(String[] args) {
